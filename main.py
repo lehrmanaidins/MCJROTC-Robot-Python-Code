8941a479776d8abb098e7684eceb62c6cd021275
lehrmanaidins <142857908+lehrmanaidins@users.noreply.github.com>
2024-02-26 10:06:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,71 +1,52 @@
 
 '''
-    Hello World Python Test
+    Main Python File
     
     File: main.py
     Author: Aidin Lehrman
-    Version: 1-22-2024 15:25:55
+    Version: 2-26-2024 10:06
 '''
 
 from pitop.processing.algorithms import process_frame_for_line
 from pitop.camera import Camera
-from pitop.camera.core.capture_actions import StoreFrame
 from pitop.robotics.blockpi_rover import BlockPiRover
 from time import sleep
 import math
 
-def main():
+def drive():
+    # robot = BlockPiRover(left_motor="M0", right_motor="M3")
+    robot = DriveController(left_motor_port="M0", right_motor_port="M3")
 
-    robot = BlockPiRover(left_motor="M0", right_motor="M3")
-
-    TURN_CONSTANT: float = 0.395
-
-    theta_radians: float = 2 * math.pi # 180 degrees
-    speed: float = 0.25 # Unit-less: Gets units from 'SPEED_CONSTANT_METERS_PER_SECOND'
-
-    # Calculates time to turn to turn 'theta_radians' with a turn radius of zero.
-    time_seconds: float = (TURN_CONSTANT) * (theta_radians) / (2.0 * speed)
-
-    for _ in range(10 * 4):
-        robot.left(speed_factor = speed, turn_radius = 0.0)
-        sleep(time_seconds)
-        robot.stop()
-        sleep(1)
-
-    '''
-    for _ in range(8):
-        robot.forward(speed_factor = 1)
-        sleep(2)
-
-        robot.left(speed_factor = 1, turn_radius = 1 / 2)
-        sleep((1 / 4) * math.pi)
+    robot.rotate(360, 5.0)
+    for _ in range(4):
+        robot.forward(1.0)
+        sleep(1.0)
+        robot.rotate(180, 2.5)
 
     robot.stop()
-    '''
 
 def process_image(frame):
     processed_data = process_frame_for_line(frame)
     processed_frame = processed_data["robot_view"]
     return processed_frame
 
-def capture_image():
+def capture_video():
 
     # process_frame_for_line(frame, image_format="PIL", process_image_width=320)
     camera = Camera()
 
-    camera.start_handling_frames(
+    camera.start_video_capture() # I think this has to be called before 'start_handling_frame' to process images properly
+    camera.start_handling_frames( # Should outline blue line on every frame
         callback_on_frame = process_image,
-        frame_interval=1,
+        frame_interval = 1, # Has to be 1 to process image every frame
     )
-    camera.start_video_capture()
-
-    sleep(5)
+    
+    sleep(5.0) # Record for 5.0 seconds
     # print(f'Is Recording: {camera.is_recording()}')
-    sleep(5)
 
     camera.stop_handling_frames()
     camera.stop_video_capture()
     # camera.capture_image(output_file_name="image_capture.png")
 
 if __name__ == '__main__':
-    capture_image()
+    capture_video()
